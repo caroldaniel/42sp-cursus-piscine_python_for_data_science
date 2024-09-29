14f6b5b99380970c9be6408a040f27c09ed46f26
Carolina Daniel <arq.carolinadaniel@gmail.com>
2024-09-29 17:38:59 -0300

    [Update] Installation file

--- python-0-starting/ex09/install.sh
+++ python-0-starting/ex09/install.sh
@@ -21,34 +21,46 @@ echo -e "${YELLOW}Cleaning previous build...${NC}"
 rm -rf build dist ft_package.egg-info > /dev/null 2>&1
 print_status "Cleaning"
 
-# Uninstall the package (if installed)
-echo -e "${YELLOW}Uninstalling existing ft_package...${NC}"
-pip uninstall -y ft_package > /dev/null 2>&1
-print_status "Uninstallation"
-
 # Build the package
 echo -e "${YELLOW}Building the package...${NC}"
 python3 setup.py sdist bdist_wheel > /dev/null 2>&1
 print_status "Build"
 
-# Install the package
-echo -e "${YELLOW}Installing the package...${NC}"
+# Install the package using tar.gz file
+echo -e "${YELLOW}Installing the package using tar.gz file...${NC}"
 pip install ./dist/ft_package-0.0.1.tar.gz > /dev/null 2>&1
-print_status "Installation"
+print_status "Installation with tar.gz file"
 
 # Show the installed package
 echo -e "${YELLOW}Displaying installed package info...${NC}"
-pip show -v ft_package > /dev/null 2>&1
-print_status "Show package"
+pip show -v ft_package
 
 # Test the package
 echo -e "${YELLOW}Running tests...${NC}"
 python3 test/test.py
 
-# Delete the package and test again
+# Delete the package
 echo -e "${YELLOW}Uninstalling the package...${NC}"
 pip uninstall -y ft_package > /dev/null 2>&1
 print_status "Uninstallation"
 
+# Install the package using whl file
+echo -e "${YELLOW}Installing the package using whl file...${NC}"
+pip install ./dist/ft_package-0.0.1-py3-none-any.whl > /dev/null 2>&1
+print_status "Installation with whl file"
+
+# Show the installed package
+echo -e "${YELLOW}Displaying installed package info...${NC}"
+pip show -v ft_package
+
+# Test the package
 echo -e "${YELLOW}Running tests again...${NC}"
+python3 test/test.py
+
+# Delete the package
+echo -e "${YELLOW}Uninstalling the package...${NC}"
+pip uninstall -y ft_package > /dev/null 2>&1
+print_status "Uninstallation"
+
+echo -e "${YELLOW}Running tests with no package installed...${NC}"
 python3 test/test.py
